c8119af6ecd939e147ab0dedd483489c2b99c22a
Luke Golden <webseedsonline@gmail.com>
2016-12-15 14:36:56 -0800

    Installed react-bootstrap and created newWorkout component

---
+++ client/components/NewWorkout.react.js
@@ -0,0 +1,30 @@
+'use strict'
+
+import Panel from 'react-bootstrap/lib/Panel';
+
+
+
+export default class NewWorkout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Panel header="Create A Workout">
+          
+        </Panel>
+      </div>
+    )
+  }
+
+
+
+
+
+
+}
