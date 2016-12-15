28bd1e54ac15d3fcc33e78240262f464cbc83c2a
Luke Golden <webseedsonline@gmail.com>
2016-12-15 14:45:21 -0800

    Add form controls - push rest to master

--- client/components/NewWorkout.react.js
+++ client/components/NewWorkout.react.js
@@ -1,8 +1,8 @@
 'use strict'
 
 import Panel from 'react-bootstrap/lib/Panel';
-
-
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+import FormControl from 'react-bootstrap/lib/FormControl';
 
 export default class NewWorkout extends React.Component {
   constructor(props) {
@@ -16,7 +16,13 @@ export default class NewWorkout extends React.Component {
     return (
       <div>
         <Panel header="Create A Workout">
-          
+          <form>
+            <FormGroup>
+              <FormControl>
+
+              </FormControl>
+            </FormGroup>
+          </form>
         </Panel>
       </div>
     )
